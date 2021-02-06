34dfdbc0c30aff1ed718f53cf3e15d2fe68394e7
kyslukha <kyslukha@gmail.com>
2021-02-06 15:40:52 +0200

    page 39

--- src/main/java/ua/com/alevel/BookService.java
+++ src/main/java/ua/com/alevel/BookService.java
@@ -7,16 +7,19 @@ import java.util.List;
 public class BookService {
 
     public static void main(String[] args) {
-        List<Album> booksSource = new ArrayList<>();
+        List<Album> albumSource = new ArrayList<>();
+        List<Album> albumDestination = new ArrayList<>();
         List<Book> booksDestination = new ArrayList<>();
-        copyToBooks(booksSource, booksDestination);
+        copyToBooks(albumSource, booksDestination);
+        copyToBooks(albumSource, albumDestination);
     }
 
-    //public static void copyToBooks(List<Book> src, List<Book> dest) {
-    public static <T> void copyToBooks(List<? extends T> src, List<? super T> dest) {
+    //public static void copyToBooks(List<? extends Book> src, List<Book> dest) {
+    public static <T extends Book> void copyToBooks(List<? extends T> src, List<? super T> dest) {
+        //   for (Book book : src) {
         for (T book : src) {
             dest.add(book);
         }
     }
-}
 
+}
